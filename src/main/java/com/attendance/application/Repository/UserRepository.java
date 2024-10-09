debd2c8a41eaadd13779d14878d224e28b77b937
NasirBhakshey <nasirbhakshey35@gmail.com>
2024-10-09 15:24:26 +0530

    fourth commit

--- src/main/java/com/attendance/application/Repository/UserRepository.java
+++ src/main/java/com/attendance/application/Repository/UserRepository.java
@@ -11,7 +11,8 @@ import com.attendance.application.Entities.User;
 public interface UserRepository extends JpaRepository<User, Integer>{
 
     User findByEmail(String email);
-    @Query("SELECT u FROM User u JOIN u.Attend a WHERE a.a_id = :a_id")
+
+    @Query("SELECT Attend.a_id, User.U_name FROM Attend INNER JOIN User ON Orders.CustomerID = Customers.CustomerID")
     List<User> findUserByAttendID(@Param("a_id") Integer a_id);
     
     @Query("SELECT u FROM User u JOIN FETCH u.attend WHERE u.u_id = :u_id")
